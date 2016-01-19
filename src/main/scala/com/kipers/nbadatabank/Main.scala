6fd68311c500f9ecca6a945a6709337a0fa60c66
Chris Kipers <ckipers@gmail.com>
2016-01-19 08:58:08 -0500

    renamed runner to main to make which class to execute more obvious. Added some documentation to main

--- src/main/scala/com/kipers/nbadatabank/Main.scala
+++ src/main/scala/com/kipers/nbadatabank/Main.scala
@@ -10,13 +10,18 @@ import rx.lang.scala.Observable
 import scala.concurrent.ExecutionContext
 import ExecutionContext.Implicits.global
 
-object Runner extends App {
+/**
+ * Builds a MongoDB database with NBA statistics pulled from stats.nba.com.
+ */
+object Main extends App {
   val InsertBatchSize = 1000
+  // Request to stats.nba.com must be throttled to prevent IP from being blocked.
   val RequestDelayInMillis = 100
 
   val StartSeasonYear = 1990
   val EndSeasonYear = 2016
 
+  // Get a list of all the seasons in the format xxxx-xx (e.i. 2015-16)
   val allSeasons = {
     (StartSeasonYear until EndSeasonYear).map(year => {
       val endingPartOfSeason = (year + 1) % 100
@@ -24,24 +29,37 @@ object Runner extends App {
     })
   }
 
+  // Latch is used to block main thread until all observables that insert results into the DB are complete
   val numberOfStreamsToBlockOn = DBCollections.values.size
   val latch = new CountDownLatch(numberOfStreamsToBlockOn)
 
+  // Create a stream of all players for every season
+  val commonPlayerStream =
+    allSeasons
+      .map(PlayerService.getAllCommonPlayerStream(_, RequestDelayInMillis))
+      .reduce(_.merge(_))
 
-  val commonPlayerStream = allSeasons.map(PlayerService.getAllCommonPlayerStream(_, RequestDelayInMillis)).reduce(_.merge(_))
   insertStreamIntoDB(commonPlayerStream, DBCollections.CommonPlayersCollection)
 
   val teamStream = TeamService.getTeamsList()
   insertStreamIntoDB(teamStream, DBCollections.TeamCollection)
 
   val teamIdStream = teamStream.map(_ ("TEAM_ID").asInstanceOf[Int])
-  val teamIdWithSeasonStream = allSeasons.map(s => teamIdStream.map(t => (t, s))).reduce(_.merge(_))
+  // Create a stream of every team with every season
+  val teamIdWithSeasonStream =
+    allSeasons
+      .map(s => teamIdStream.map(t => (t, s)))
+      .reduce(_.merge(_))
 
-  val rosterStreams = teamIdWithSeasonStream.flatMap { case (teamId, season) => TeamService.getTeamRosterStreams(teamId, season, RequestDelayInMillis) }
+  // Stream of player and coach rosters for every team and season
+  val rosterStreams =
+    teamIdWithSeasonStream
+      .flatMap { case (teamId, season) => TeamService.getTeamRosterStreams(teamId, season, RequestDelayInMillis) }
+
   val commonRosterStream = TeamService.getTeamRosterStream(rosterStreams, TeamRosterStreamType.CommonTeamRoster)
-  val coachRosterStream = TeamService.getTeamRosterStream(rosterStreams, TeamRosterStreamType.Coaches)
-
   insertStreamIntoDB(commonRosterStream, DBCollections.TeamRosterCollection)
+
+  val coachRosterStream = TeamService.getTeamRosterStream(rosterStreams, TeamRosterStreamType.Coaches)
   insertStreamIntoDB(coachRosterStream, DBCollections.CoachRosterCollection)
 
   val gameLogStream = teamIdWithSeasonStream.flatMap(ts =>
@@ -61,12 +79,20 @@ object Runner extends App {
 
   latch.await()
 
-  def insertStreamIntoDB(stream: Observable[NbaResult], dbCollection: DBCollections) {
+  /**
+   * Inserts a stream of NbaResults into a mongodb collection and decrements the latch. Results are inserted in batches
+   * for performance.
+   * @param stream The stream of results to be inserted into MongoDB.
+   * @param dbCollection The collection the results will be inserted into.
+   */
+  private def insertStreamIntoDB(stream: Observable[NbaResult], dbCollection: DBCollections) {
     val dbCollectionName = dbCollection.toString
     stream
       .tumblingBuffer(InsertBatchSize)
-      .doOnCompleted(latch.countDown())
-      .doOnCompleted(println(s"Done inserted all $dbCollectionName"))
+      .doOnCompleted({
+        println(s"Done inserted all $dbCollectionName")
+        latch.countDown()
+      })
       .zipWithIndex
       .subscribe(resultsWithIndex => {
         val (results, indexOfBatch) = resultsWithIndex
